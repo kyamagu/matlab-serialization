3d36a1973c68f75c1e556368c0eb42af927ce893
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2013-10-28 16:06:51 -0400

    Added type check

--- deserialize.m
+++ deserialize.m
@@ -7,5 +7,8 @@ function obj = deserialize(bytes)
 %
 % See also serialize
     loadlibmx();
+    if ~isa(bytes, 'uint8')
+      error('Input must be uint8: %s', class(bytes));
+    end
     obj = calllib('libmx', 'mxDeserialize', bytes, numel(bytes));
 end
